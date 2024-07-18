2db6e4c59041a99294e382f1887843578e03a8d3
Darren Teh <58021556+TYM-Darren@users.noreply.github.com>
2024-07-18 17:43:54 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app2 -v c:\\Users\\darre\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-# docker run -d -p 80:80 --name my-apache-php-app -v /home/darren/SSD/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+#docker run -d -p 80:80 --name my-apache-php-app2 -v c:\\Users\\darre\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/darren/SSD/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
